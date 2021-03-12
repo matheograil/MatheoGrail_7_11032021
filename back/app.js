2df32405ea0a3e01e8e853dfde609d20046e99dd
Mathéo Grail <grail.matheo@protonmail.com>
2021-03-12 13:42:36 +0100

    Initialisation du projet

---
+++ back/app.js
@@ -0,0 +1,13 @@
+// Framework Express.
+const express = require('express');
+const app = express();
+
+// Cross Origin Resource Sharing.
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app;

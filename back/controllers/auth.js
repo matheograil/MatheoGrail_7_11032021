e2e543f0fa15e17a2aeca177d3d034e502a07fc4
Mathéo Grail <grail.matheo@protonmail.com>
2021-03-15 14:22:28 +0100

    API : inscription

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -21,7 +21,7 @@ exports.register = (req, res) => {
         const password = req.body.password;
         // L'utilisateur existe-t-il ?
         User.findOne({ where: { email: email } }).then((user) => {
-            if (user ==! null) {
+            if (user !== null) {
                 return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
             }
             // Chiffrement du mot de passe.
@@ -38,3 +38,41 @@ exports.register = (req, res) => {
         }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
     }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
 };
+
+// POST : api/auth/login.
+exports.login = (req, res) => {
+    const UserValidator = new Validator(req.body, {
+        email: 'required|email|maxLength:50',
+        password: 'required|string|lengthBetween:10,100'
+    });
+    // Vérification des données reçues.
+    UserValidator.check().then(matched => {
+        if (!matched) {
+            return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+        }
+        // Définition des variables.
+        const email = req.body.email;
+        const password = req.body.password;
+        // L'utilisateur existe-t-il ?
+        User.findOne({ where: { email: email } }).then((user) => {
+            if (user === null) {
+                return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+            }
+            // Le mot de passe correspond-t-il ?
+            bcrypt.compare(password, user.password).then(valid => {
+                if (!valid) {
+                    return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+                }
+                // Enregistrement du jeton d'accès.
+                res.status(200).json({
+                    userId: user.id,
+                    token: jsonwebtoken.sign(
+                        { userId: user.id },
+                        process.env.JWT_TOKEN,
+                        { expiresIn: '12h' }
+                    )
+                });
+            }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+        }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+    }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+}

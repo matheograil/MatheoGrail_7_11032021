c9fd33c942dbf663337d16a858883bf5ec693b6f
Mathéo Grail <grail.matheo@protonmail.com>
2021-03-22 10:44:25 +0100

    Update messages.js

--- back/routes/messages.js
+++ back/routes/messages.js
@@ -18,15 +18,15 @@ const messagesCtrl = require('../controllers/messages');
  * Les différentes fonctions de notre route.
  */
 // Publication d'un message.
-router.post('/', auth, multer, messagesCtrl.newMessage);        /* POST : api/publication/messages */
+router.post('/', auth, multer, messagesCtrl.newMessage);    /* POST : api/publication/messages */
 // Affichage des messages.
-router.get('/', auth, messagesCtrl.getAllMessages);     /* GET : api/publication/messages */
+router.get('/', auth, messagesCtrl.getAllMessages);         /* GET : api/publication/messages */
 // Affichage d'un message.
-router.get('/:id', auth, messagesCtrl.getMessage);      /* GET : api/publication/messages/:id */
+router.get('/:id', auth, messagesCtrl.getMessage);          /* GET : api/publication/messages/:id */
 // Modification d'un message.
-router.put('/:id', auth, messagesCtrl.editMessage);     /* PUT : api/publication/messages/:id */
+router.put('/:id', auth, messagesCtrl.editMessage);         /* PUT : api/publication/messages/:id */
 // Suppression d'un message.
-router.delete('/:id', auth, messagesCtrl.delMessage);   /* DELETE : api/publication/messages/:id */
+router.delete('/:id', auth, messagesCtrl.delMessage);       /* DELETE : api/publication/messages/:id */
 
 
 module.exports = router;

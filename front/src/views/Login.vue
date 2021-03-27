f5e82f0ac1f24af58f708b35a96e3793d97aedc1
Mathéo <grail.matheo@protonmail.com>
2021-03-27 16:11:13 +0100

    Inscription en cours de dev (2)

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,16 +1,43 @@
 <template>
-    <div class="auth">
-        <h2 class="auth__title">Connectez-vous pour continuer...</h2>
-        <div class="auth__form">
-            <div class="auth__inputs">
-                <input class="auth__input" type="email" name="email" id="email" placeholder="Adresse électronique">
-                <input class="auth__input" type="password" name="password" id="password" placeholder="Mot de passe">
+    <div class='auth'>
+        <h2 class='auth__title'>Connectez-vous pour continuer...</h2>
+        <div class='auth__form'>
+            <div class='auth__inputs'>
+                <input class='auth__input' v-model='email' placeholder='Adresse électronique'>
+                <input class='auth__input' type='password' v-model='password' placeholder='Mot de passe'>
             </div>
         </div>
-        <a class="auth__go" type="button">Se connecter</a>
+        <a class='auth__go' v-on:click='login' type='button'>Se connecter</a>
     </div>
 </template>
 
-<style lang="scss">
-    @import "../styles/auth.scss"
+<script>
+    export default {
+        data: function () {
+            return {
+                email: null,
+                password: null
+            }
+        },
+        methods: {
+            login() {
+                // Déclaration des variables.
+                const email = this.email,
+                password = this.password
+
+                // Vérification des variables.
+                const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!email || !emailRegex.test(String(email).toLowerCase()) || email.length > 50 ||
+                    !password || typeof password !== 'string' || password.length > 100 || password.length < 10) {
+                    return false
+                }
+
+                // Envoie des données à l'API.
+            }
+        }
+    }
+</script>
+
+<style lang='scss'>
+    @import '../styles/auth.scss'
 </style>
--- front/src/views/Register.vue
+++ front/src/views/Register.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="auth">
-        <h2 class="auth__title">Vous pouvez aussi vous inscrire !</h2>
-        <div class="auth__form">
-            <div class="auth__inputs">
-                <input class="auth__input" type="text" v-model="firstName" placeholder="Prénom">
-                <input class="auth__input" type="text" v-model="lastName" placeholder="Nom">
-                <input class="auth__input" type="email" v-model="email" placeholder="Adresse électronique">
-                <input class="auth__input" type="password" v-model="password" placeholder="Mot de passe">
-                <input class="auth__input" type="password" v-model="passwordConfirmation" placeholder="Mot de passe">
+    <div class='auth'>
+        <h2 class='auth__title'>Vous pouvez aussi vous inscrire !</h2>
+        <div class='auth__form'>
+            <div class='auth__inputs'>
+                <input class='auth__input' v-model='firstName' placeholder='Prénom'>
+                <input class='auth__input' v-model='lastName' placeholder='Nom'>
+                <input class='auth__input' v-model='email' placeholder='Adresse électronique'>
+                <input class='auth__input' type='password' v-model='password' placeholder='Mot de passe'>
+                <input class='auth__input' type='password' v-model='passwordConfirmation' placeholder='Mot de passe'>
             </div>
         </div>
-        <a class="auth__go" v-on:click="register" type="button">S'inscrire</a>
+        <a class='auth__go' v-on:click='register' type='button'>S'inscrire</a>
     </div>
 </template>
 
@@ -27,22 +27,29 @@
         },
         methods: {
             register() {
+                // Déclaration des variables.
                 const firstName = this.firstName,
                 lastName = this.lastName,
                 email = this.email,
                 password = this.password,
                 passwordConfirmation = this.passwordConfirmation
 
-                if (!firstName || !lastName || !email || !password || !passwordConfirmation) {
+                // Vérification des variables.
+                const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!firstName || typeof firstName !== 'string' || firstName.length > 50 ||
+                    !lastName || typeof lastName !== 'string' || lastName.length > 50 ||
+                    !email || !emailRegex.test(String(email).toLowerCase()) || email.length > 50 ||
+                    !password || typeof password !== 'string' || password.length > 100 || password.length < 10 ||
+                    password !== passwordConfirmation) {
                     return false
                 }
 
-                alert('hey')
+                // Envoie des données à l'API.
             }
         }
     }
 </script>
 
-<style lang="scss">
-    @import "../styles/auth.scss"
+<style lang='scss'>
+    @import '../styles/auth.scss'
 </style>

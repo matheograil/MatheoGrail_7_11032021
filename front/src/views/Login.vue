b35e62feee9e404f8e11feac0e6fbf3cc9e6fddf
Mathéo <grail.matheo@protonmail.com>
2021-03-27 13:20:48 +0100

    DRY

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,56 +1,16 @@
 <template>
-    <div class="login">
-        <h2 class="login__title">Connectez-vous pour continuer...</h2>
-        <div class="login__form">
-            <div class="login__inputs">
-                <input class="login__input" type="email" name="email" id="email" placeholder="Adresse électronique">
-                <input class="login__input" type="password" name="password" id="password" placeholder="Mot de passe">
+    <div class="auth">
+        <h2 class="auth__title">Connectez-vous pour continuer...</h2>
+        <div class="auth__form">
+            <div class="auth__inputs">
+                <input class="auth__input" type="email" name="email" id="email" placeholder="Adresse électronique">
+                <input class="auth__input" type="password" name="password" id="password" placeholder="Mot de passe">
             </div>
         </div>
-        <a class="login__go" type="button">Se connecter</a>
+        <a class="auth__go" type="button">Se connecter</a>
     </div>
 </template>
 
 <style lang="scss">
-    .login {
-        .login__go {
-            margin-top: 5px;
-            background-color: #4CAF50;
-            color: white;
-            padding: 15px 32px;
-            display: inline-block;
-            font-size: 16px;
-            border-radius: 10px;
-            font-weight: bold;
-            cursor:pointer;
-        }
-
-        .login__form {
-            display: flex;
-            justify-content: center;
-
-            .login__inputs {
-                margin-top: 10px;
-                width: 400px;
-
-                .login__input {
-                    outline-style: none;
-                    margin: 5px;
-                    width: calc(100% - 20px);
-                    -webkit-appearance: none;
-                    padding: 15px;
-                    border: 1px solid #2c3e50;
-                    border-radius: 20px;
-                    font-size: 15px;
-                    font-family: Helvetica;
-                    box-sizing: border-box;
-                }
-
-                .login__input::placeholder {
-                    color: #2c3e50;
-                    opacity: 1;
-                }
-            }
-        }
-    }
+    @import "../styles/auth.scss";
 </style>
--- front/src/views/Register.vue
+++ front/src/views/Register.vue
@@ -1,59 +1,19 @@
 <template>
-    <div class="register">
-        <h2 class="login__title">Vous pouvez aussi vous inscrire !</h2>
-        <div class="register__form">
-            <div class="register__inputs">
-                <input class="register__input" type="text" name="firstName" id="firstName" placeholder="Prénom">
-                <input class="register__input" type="text" name="lastName" id="lastName" placeholder="Nom">
-                <input class="register__input" type="email" name="email" id="email" placeholder="Adresse électronique">
-                <input class="register__input" type="password" name="password" id="password" placeholder="Mot de passe">
-                <input class="register__input" type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="Mot de passe">
+    <div class="auth">
+        <h2 class="auth__title">Vous pouvez aussi vous inscrire !</h2>
+        <div class="auth__form">
+            <div class="auth__inputs">
+                <input class="auth__input" type="text" name="firstName" id="firstName" placeholder="Prénom">
+                <input class="auth__input" type="text" name="lastName" id="lastName" placeholder="Nom">
+                <input class="auth__input" type="email" name="email" id="email" placeholder="Adresse électronique">
+                <input class="auth__input" type="password" name="password" id="password" placeholder="Mot de passe">
+                <input class="auth__input" type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="Mot de passe">
             </div>
         </div>
-        <a class="register__go" type="button">S'inscrire</a>
+        <a class="auth__go" type="button">S'inscrire</a>
     </div>
 </template>
 
 <style lang="scss">
-    .register {
-        .register__go {
-            margin-top: 5px;
-            background-color: #4CAF50;
-            color: white;
-            padding: 15px 32px;
-            display: inline-block;
-            font-size: 16px;
-            border-radius: 10px;
-            font-weight: bold;
-            cursor:pointer;
-        }
-
-        .register__form {
-            display: flex;
-            justify-content: center;
-
-            .register__inputs {
-                margin-top: 10px;
-                width: 400px;
-
-                .register__input {
-                    outline-style: none;
-                    margin: 5px;
-                    width: calc(100% - 20px);
-                    -webkit-appearance: none;
-                    padding: 15px;
-                    border: 1px solid #2c3e50;
-                    border-radius: 20px;
-                    font-size: 15px;
-                    font-family: Helvetica;
-                    box-sizing: border-box;
-                }
-
-                .register__input::placeholder {
-                    color: #2c3e50;
-                    opacity: 1;
-                }
-            }
-        }
-    }
+    @import "../styles/auth.scss";
 </style>

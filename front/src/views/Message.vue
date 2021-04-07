7afbc3c5a231a69e7a51534dc782488d4a33b3a9
Mathéo <grail.matheo@protonmail.com>
2021-04-07 10:22:32 +0200

    Update Message.vue

--- front/src/views/Message.vue
+++ front/src/views/Message.vue
@@ -7,8 +7,8 @@
                 <div class='messages__more'>Publié par <strong>{{ author }}</strong> le {{ timestamp }} →</div>
                 {{ content }}
                 <img class='messages__img' v-if='imageUrl' v-bind:src='imageUrl'/>
-                <a class='btn btn-primary' v-if='authorId == this.userId' v-on:click='isInProgress = 1'>Modifier</a>
-                <a class='btn btn-error' v-if='authorId == this.userId || isAdmin' v-on:click='remove'>Supprimer</a>
+                <a class='btn btn-primary' v-if='authorId == userId' v-on:click='isInProgress = 1'>Modifier</a>
+                <a class='btn btn-error' v-if='authorId == userId || isAdmin' v-on:click='removeMessage'>Supprimer</a>
             </div>
         </div>
         <div class='form' v-else>
@@ -25,6 +25,7 @@
             <div class='messages__content'>
                 <div class='messages__more'>Publié par <strong>{{ comment.author }}</strong> le {{ comment.timestamp }} →</div>
                 {{ comment.content }}
+                <a class='btn btn-error' v-if='this.authorId === comment.userId'>Supprimer</a>
             </div>
         </div>
     </div>
@@ -108,7 +109,7 @@
                     })
             },
             // Suppression du message et de ses commentaires.
-            remove() {
+            removeMessage() {
                 const requestOptions = {
                     method: 'DELETE',
                     headers: { 'authorization_token': this.authorizationToken, 'user_id': this.userId }

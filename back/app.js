5e3ff3d3010369ceef87cb6ae8d5a11eadcc835f
Mathéo <grail.matheo@protonmail.com>
2021-04-15 14:21:23 +0200

    Update app.js

--- back/app.js
+++ back/app.js
@@ -15,7 +15,7 @@ app.listen(3000,() => {
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, authorization_token, user_id');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 

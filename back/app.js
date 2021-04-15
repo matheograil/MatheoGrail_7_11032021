d963ee0c77ee933f117efb8daeaf98bfa07b6402
Mathéo <grail.matheo@protonmail.com>
2021-04-15 14:20:51 +0200

    Problème des Headers sur Chrome résolu

--- back/app.js
+++ back/app.js
@@ -14,7 +14,7 @@ app.listen(3000,() => {
 // À SUPPRIMER DANS UN ENVIRONNEMENT DE PRODUCTION.
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, authorization_token, user_id');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
